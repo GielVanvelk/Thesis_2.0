577edd50fbe55a796623629caebf39001890082a
giel <giel.vanvelk@student.kuleuven.be>
2021-06-14 15:50:42 +0200

    Initial commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/giel/etasl/ws/giel_workspace/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/giel/etasl/ws/etasl-rtt/devel;/home/giel/etasl/ws/etasl/devel;/opt/ros/kinetic'.split(';') if '/home/giel/etasl/ws/etasl-rtt/devel;/home/giel/etasl/ws/etasl/devel;/opt/ros/kinetic' != '' else []
